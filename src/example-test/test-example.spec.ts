725f3f870d90f594388f5fd40ada097c636fdb8b
TonyThorne <anthony.thorne@gmail.com>
2022-02-01 20:35:51 +0000

    Updated filenames

---
+++ src/example-test/test-example.spec.ts
@@ -0,0 +1,7 @@
+import { fn } from './exampleTypescript'
+
+describe('Check basic Tests are working', () => {
+    it('should test return, this is a test', () => {
+        expect(fn()).toEqual('This is a test')
+    })
+})
