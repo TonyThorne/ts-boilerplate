422950d0598375670cb225a75162ebeac65e85f4
TonyThorne <anthony.thorne@gmail.com>
2022-02-03 10:02:36 +0000

    Minor changes to the JSON

--- src/example-test/test-json-imports.spec.ts
+++ src/example-test/test-json-imports.spec.ts
@@ -2,6 +2,11 @@ import { loadJSON } from './test-json-import'
 
 describe('Example loading a JSON file into Typescript', () => {
     it('should load a JSON file into typescript file', () => {
-        expect(loadJSON()).toEqual({ example: 'object' })
+        const data = loadJSON()
+        expect(data.forename).toBeDefined()
+        expect(data).toHaveProperty('forename')
+
+        expect(data.surname).toEqual('Doe')
+        // expect(new Set(data )).toContain('John')
     })
 })
