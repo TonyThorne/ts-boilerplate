d614fc9033ca7dcf71c33668d96b64746c9a5b82
Tony Thorne <anthony.thorne@gmail.com>
2023-04-01 15:23:28 +0100

    Feat: Mapped activities to their detail objects

--- src/rbac/activity-search.ts
+++ src/rbac/activity-search.ts
@@ -26,10 +26,12 @@ function searchActivitiesByCode(activityCode: string): Activity[] {
     return data.filter((activity) => activity["Activity Code"].toLowerCase() === activityCode.toLowerCase());
 }
 
+// search by any key
 function searchActivitiesByKey(key: keyof Activity, value: string): Activity[] {
     return data.filter((activity) => activity[key].toLowerCase() === value.toLowerCase());
 }
 
+// limit which fields are returned
 function filterFields<T>(items: T[], fields: (keyof T)[]): Partial<T>[] {
     return items.map((item) => {
         const filteredItem: Partial<T> = {};
@@ -43,8 +45,8 @@ function filterFields<T>(items: T[], fields: (keyof T)[]): Partial<T>[] {
 //Activity Sub Group Code": "C0079",
 // "Activity Group Code": "D8002",
 // Example usage:
-const objectKey: keyof Activity = "Activity Code";
-const searchValue = 'B0825';
+const objectKey: keyof Activity = "Activity Description";
+const searchValue = 'B0428';
 const fieldsToInclude: (keyof Activity)[] = ["Activity Name", "Activity Code"];
 const searchResults = searchActivitiesByKey(objectKey, searchValue);
 const filteredResults = filterFields(searchResults, fieldsToInclude);
--- src/rbac/job-roles-consolidate.ts
+++ src/rbac/job-roles-consolidate.ts
@@ -1,10 +1,16 @@
-import * as data from '../data/rbac-job-roles-baseline.json';
+import * as data from '../data/rbac-job-roles-baseline.json'
+import * as rbacActivities from '../data/rbac-active-activities.json'
+
+// The purpose of this function is to consolidate the activities into a single array
+// It removes any of the columns (validPrefixes) that are not used
+
 
 type InputObject = {
     [key: string]: string | null | undefined;
 };
 
-type ResultObject = InputObject & { activities: string[] };
+type ResultObject = InputObject & { activities: string[]; associatedActivities?: ActivityObject[] };
+
 
 const validPrefixes = [
     "D0002", "D0003", "D0004", "D0006", "D0010", "D0001", "D0009", "D0012", "D0013",
@@ -12,10 +18,12 @@ const validPrefixes = [
     "T0850", "T1580", "T1670", "T2200", "T6080", "T6090", "T6092", "T1590", "T2250", "T6091",
 ];
 
+// This function is used to determine if the key starts with any of the valid prefixes
 function startsWithValidPrefix(key: string): boolean {
     return validPrefixes.some((prefix) => key.startsWith(prefix));
 }
 
+// This function is used to consolidate the activities into a single array
 function consolidateActivities(obj: InputObject): any {
     const result: any = {};
 
@@ -40,102 +48,59 @@ function consolidateActivities(obj: InputObject): any {
     return result as ResultObject;
 }
 
+// Search Job Roles
+function searchRolesByKey(key: keyof InputObject, value: string): InputObject[] {
+    return data.filter((role) => role[key].toLowerCase() === value.toLowerCase());
+}
+
 // Example usage
 const inputArray: InputObject[] = data
 
-// const inputArray: InputObject[] = [
-//     {
-//         Type: "NWD",
-//         "Staff Group": "M&D",
-//         "Staff Group Code": "S0010",
-//         "D0002 Primary Care": "",
-//         "D0003 Prescribing and Pharmacy Related": "",
-//         "D0004 Secondary Care": "B0021 B0028 B0031 B0051 B0275 B0640 B0680 B0710 B0740 B0780 B0805 B0870 B0915 B0940 B0960 B1035",
-//         "T0850 Imaging AoW": "B0035",
-//         "T1580 Primary Care AoW": "",
-//     },
-//     // ... other objects
-// ];
 
 const consolidatedObjects: ResultObject[] = inputArray.map(consolidateActivities);
-console.log(consolidatedObjects);
+// console.log(consolidatedObjects.length);
 
+// Search for a particular Job Role (by any key), then consolidate the activities so only include the included activities
+const t: ResultObject[] = searchRolesByKey("Job Role Code", "R5110").map(consolidateActivities);
+console.log(t.length);
 
-// import * as data from '../data/rbac-job-roles-baseline.json';
+// TO-DO These needs to be imported from types file
+type ActivityObject = {
+    "Activity Group Name": string;
+    "Activity Group Code": string;
+    "Activity Sub Group Name": string;
+    "Activity Sub Group Code": string;
+    "Activity Name": string;
+    "Activity Code": string;
+    "Includes": string;
+    "Activity Description": string;
+    "Restricted": string;
+    "Rationalisation Status": string;
+};
 
-// type InputObject = {
-//     [key: string]: string | null | undefined;
-// };
+// Map the consolidated activities to a new array with the activity details
+function findAssociatedActivities(consolidatedObjects: ResultObject[]): ResultObject[] {
+    for (const consolidatedObject of consolidatedObjects) {
+        const associatedActivities: ActivityObject[] = [];
 
-// type ResultObject = InputObject & { activities: string[] };
+        if (!consolidatedObject.activities) {
+            continue;
+        }
 
-// const validPrefixes = [
-//     "D0002", "D0003", "D0004", "D0006", "D0010", "D0001", "D0009", "D0012", "D0013",
-//     "D8002", "D8003", "D8004", "D8005", "D8006", "D8001", "D0008", "D0011",
-//     "T0850", "T1580", "T1670", "T2200", "T6080", "T6090", "T6092", "T1590", "T2250", "T6091",
-// ];
+        for (const activityCode of consolidatedObject.activities) {
+            const activityObject = rbacActivities.find(activity => activity["Activity Code"] === activityCode);
 
-// function startsWithValidPrefix(key: string): boolean {
-//     return validPrefixes.some((prefix) => key.startsWith(prefix));
-// }
+            if (activityObject) {
+                associatedActivities.push(activityObject);
+            }
+        }
 
-// function consolidateActivities(obj: InputObject): any {
-//     const result: any = { ...obj, activities: [] };
+        consolidatedObject.associatedActivities = associatedActivities;
+    }
 
-//     for (const key in obj) {
-//         if (startsWithValidPrefix(key) && obj[key] !== null && obj[key] !== undefined) {
-//             const activityList = (obj[key] as string).split(' ').filter(item => item !== '');
-//             result.activities.push(...activityList);
-//             delete result[key];
-//         }
-//     }
+    return consolidatedObjects;
+}
 
-//     return result;
-// }
-
-// // Example usage
-// // const input: InputObject = data
-// const input: InputObject = {
-//     "Type": "NWD",
-//     "Staff Group": "M&D",
-//     "Staff Group Code": "S0010",
-//     "Staff Sub Group": "Management - M&D",
-//     "Staff Sub Group Code": "G0010",
-//     "Job Role": "Medical Director",
-//     "Job Role Code": "R0010",
-//     "Job Role Description": "",
-//     "Restricted": "",
-//     "Rationalisation Status": "Withdrawn - when not supported use: R8000 Clinical Practitioner Access Role",
-//     "D0002 Primary Care": "",
-//     "D0003 Prescribing and Pharmacy Related": "",
-//     "D0004 Secondary Care": "B0021 B0028 B0031 B0051 B0275 B0640 B0680 B0710 B0740 B0780 B0805 B0870 B0915 B0940 B0960 B1035",
-//     "D0006 Information Governance": "",
-//     "D0010 Patient Demographics": "",
-//     "D0001 Primary and Secondary Care": "",
-//     "D0009 Orders & Results": "B0026 B0052 B1615 B1656 B1657 B1660 B1661 B1662 B1665 B1666 B1667",
-//     "D0012 Child Health": "",
-//     "D0013 Health Records": "",
-//     "D8002 Patient Records": "B0620 B0825",
-//     "D8003 Patient Administration": "B0600",
-//     "D8004 Tests/Investigations": "",
-//     "D8005 Prescribing and Pharmacy": "",
-//     "D8006 Management": "B0862",
-//     "D8001 Reports": "",
-//     "D0008 Miscellaneous": "",
-//     "D0011 Diagnostic Imaging": "B0034 B0040",
-//     "T0850 Imaging AoW": "B0035",
-//     "T1580 Primary Care AoW": "",
-//     "T1670 General Practice AoW": "",
-//     "T2200 Pharmacy AoW": "",
-//     "T6080 Mental Health AoW": "",
-//     "T6090 PCT Admin AoW": "",
-//     "T6092 Secondary Care AoW": "",
-//     "T1590 Community Health AoW": "",
-//     "T2250 Accident & Emergency AoW": "",
-//     "T6091 National Back Office AoW": "",
-//     "": "",
-//     "(more Areas of Work may be added as required - first five letters of this row must be AoW Tertiary Code followed by space)\r": "\r"
-// };
-
-// const consolidatedObject: ResultObject = consolidateActivities(input);
-// console.log(consolidatedObject);
+// Example usage:
+const allAssociatedActivities = findAssociatedActivities(t);
+console.log(allAssociatedActivities[0]);
